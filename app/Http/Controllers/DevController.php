6587b369c8ebaa6e663e3d221678badafe6a16e2
Dale Carter <xthedalex@gmail.com>
2021-01-05 20:30:28 -0500

    Branding Changes Admin Protection

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -3,26 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Models\Event;
-use App\Models\Driver;
-use App\Models\SeriesLock;
-use App\Models\DriverScore;
-use Illuminate\Support\Str;
-use App\Http\Guzzle\Sgp\SgpBase;
-use App\DataProvider\DataProvider;
 use Illuminate\Support\Facades\Auth;
-use App\Helper\DriverScoreCalculator;
-use App\Notifications\TestNotification;
-use NotificationChannels\Discord\Discord;
-use App\Notifications\DiscordNotification;
-use App\SingleUseFeatures\AbandondedMembers;
-use Carbon\Carbon;
 
 class DevController extends Controller
 {
     public function __construct()
     {
-        //$this->middleware('role:admin');
+        $this->middleware('role:admin');
     }
 
     public function index()
--- resources/views/layout/_sidebar.blade.php
+++ resources/views/layout/_sidebar.blade.php
@@ -3,7 +3,7 @@
             <div class="iq-sidebar-logo d-flex justify-content-between">
                 <a href="{{ route('home') }}">
                     <img src="{{ asset('images/logo.png') }}" class="img-fluid" alt="">
-                    <span>ACCSSA</span>
+                    <span>NWSR</span>
                 </a>
                 <div class="iq-menu-bt align-self-center">
                     <div class="wrapper-menu">
