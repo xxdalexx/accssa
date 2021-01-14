e64c2713c44be78aeb1f3fa859bb9676b5fd2fdc
Dale Carter <xthedalex@gmail.com>
2021-01-14 05:22:35 -0500

    Series info typo fix.

--- resources/views/series/info/6.blade.php
+++ resources/views/series/info/6.blade.php
@@ -1,4 +1,4 @@
 <h4>Series Info</h4>
-<p>Registration Closes Jan 17th.</p>
+<p>Registration Closes Jan 24th.</p>
 <p>25 Minute Quali - First 10 minutes for the Pro split, second 15 minutes for Silver and Am.</p>
 </div></div><div class="iq-card"><div class="iq-card-body">
--- resources/views/series/info/9.blade.php
+++
@@ -1,4 +0,0 @@
-<h4>Series Info</h4>
-<p>Registration Closes Jan 17th.</p>
-<p>25 Minute Quali - First 10 minutes for the Pro split, second 15 minutes for Silver and Am.</p>
-</div></div><div class="iq-card"><div class="iq-card-body">
