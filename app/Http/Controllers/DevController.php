b85eab6519e4a72a4aea8af49659d3a334fc8ee7
Dale Carter <xthedalex@gmail.com>
2021-01-28 18:05:04 -0500

    Reenable some security.

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -3,23 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Models\Driver;
-use App\Models\Series;
-use App\Http\Guzzle\Sgp\SgpBase;
-use App\Http\Guzzle\Sgp\SgpPost;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Notification;
-use App\Notifications\SeriesStartNotification;
-use App\Http\Guzzle\Sgp\RequestBuilders\RemoveDriverFromEvent;
-use App\SingleUseFeatures\MembersMissingDiscordOnSgp;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 class DevController extends Controller
 {
     public function __construct()
     {
-        //$this->middleware('role:admin');
+        $this->middleware('role:admin');
     }
 
     public function index()
